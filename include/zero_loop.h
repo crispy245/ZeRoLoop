e19b06217bdfab6d98a0ea89bc655cf68f62d723
aldo <aldobalsamoreyes@gmail.com>
2024-11-12 16:26:50 +0800

    Refactor a little

--- include/zero_loop.h
+++ include/zero_loop.h
@@ -3,230 +3,213 @@
 #include "bit_vector.h"
 #include "bit.h"
 #include "ram.h"
+#include <iostream>
 
-struct ZeRoLoop
-{
+struct ZeRoLoop {
+    struct Register {
+        vector<bit> data;
 
-        struct Register
-        {
-                vector<bit> data;
+        Register(size_t width = 32) : data(width, bit(0)) {}
+        Register(vector<bit> &bits) : data(bits) {}
 
-                Register(size_t width = 32) : data(width, bit(0)) {}
-                Register(vector<bit> &bits) : data(bits) {}
-                // TODO Init register with a value, could use it for immediates...
-
-                bit get_bit(size_t index)
-                {
-                        return data[index];
-                }
-        };
-
-        struct RegisterFile
-        {
-                vector<Register> register_list;
-
-                RegisterFile(size_t num_registers = 32, size_t width = 32)
-                    : register_list(num_registers, Register(width)) {}
-        };
-
-        // Read from register, Handles x0
-        static Register register_read(RegisterFile &x, bigint L, bigint H, Register &i, bigint ibits)
-        {
-                if (H <= L)
-                        return Register{};
-                if (H == L + 1)
-                        return x.register_list.at(L);
-
-                bigint splitpos = 0;
-                bigint split = 1;
-                while (L + split < H - split)
-                {
-                        splitpos += 1;
-                        split *= 2;
-                }
-                // now H-L <= split+split
-                // and split == 2^splitpos
-
-                // conventional RAM circuit:
-                // use i[0:splitpos] to look up entry in x[L:L+split]
-                //   result0 = x[L+(I mod split)]
-                // use i[0:splitpos] to look up entry in x[L+split:H], called result1
-                //   result1 = x[L+split+(I mod split)] if L+split+(I mod split) < H
-                // multiplex according to i[splitpos]
-
-                // why this works:
-                // assume I < H-L; then I < split+split
-                // case 0: i[splitpos] is 0
-                //   then I < split
-                //   so result0 = x[L+I], and we'll select result0
-                // case 1: i[splitpos] is 1
-                //   then split <= I < H-L <= split+split
-                //   so split+(I mod split) = I <= H-L
-                //   so result1 = x[L+I], and we'll select result1
-
-                if (ibits <= splitpos)
-                {
-                        // different case: definitely want result from x[L:L+split], no multiplexing
-                        return register_read(x, L, L + split, i, ibits);
-                }
-
-                Register result0 = register_read(x, L, L + split, i, splitpos);
-                Register result1 = register_read(x, L + split, H, i, splitpos);
-
-                assert(result0.data.size() == result1.data.size());
-
-                bit isplit = i.data.at(splitpos);
-
-                Register result(0);
-                for (bigint r = 0; r < result0.data.size(); ++r)
-                {
-                        bit x0 = result0.data.at(r);
-                        bit x1 = result1.data.at(r);
-                        result.data.push_back(isplit.mux(x0, x1));
-                }
-
-                return result;
-        }
-        static Register register_read(
-            RegisterFile &x,
-            bigint L,
-            bigint H,
-            Register &i)
-        {
-                return register_read(x, L, H, i, i.data.size());
+        bit get_bit(size_t index) const {
+            return data[index];
         }
 
-        static Register register_read(
-            RegisterFile &x,
-            Register &i)
-        {
-                return register_read(x, 0, x.register_list.size(), i);
+        // Get number of bits in register
+        size_t width() const {
+            return data.size();
         }
 
-        static void register_write(
-            RegisterFile &x, bigint L, bigint H, Register &i, bigint ibits,
-            Register &write_data, bit b = bit(1), bool top = 1) // Added defaults here
-        {
-                assert(x.register_list[0].data.size() == write_data.data.size());
-                if (H <= L)
-                        return;
-                if (H == L + 1)
-                {
-                        if (top)
-                                x.register_list.at(L) = write_data;
-                        else
-                                for (bigint r = 0; r < write_data.data.size(); ++r)
-                                        x.register_list.at(L).data.at(r) =
-                                            b.mux(write_data.data.at(r), x.register_list.at(L).data.at(r));
-                        return;
-                }
-                bigint splitpos = 0;
-                bigint split = 1;
-                while (L + split < H - split)
-                {
-                        splitpos += 1;
-                        split *= 2;
-                }
-                if (ibits <= splitpos)
-                {
-                        return register_write(x, L, L + split, i, ibits, write_data, b, top);
-                }
-                bit isplit = i.data.at(splitpos);
-                register_write(x, L, L + split, i, splitpos, write_data, top ? isplit : (b | isplit), 0);
-                register_write(x, L + split, H, i, splitpos, write_data, top ? ~isplit : b.orn(isplit), 0);
+        // Print register contents
+        void print(const std::string &name) const {
+            std::cout << name << ": ";
+            for (int i = width() - 1; i >= 0; i--) {
+                std::cout << data[i].value();
+            }
+            std::cout << std::endl;
         }
 
-        static void register_write(
-            RegisterFile &x,
-            bigint L,
-            bigint H,
-            Register &i,
-            Register &data)
-        {
-                register_write(x, L, H, i, i.data.size(), data); // Default b and top will be used
+        // Access individual bits with bounds checking
+        bit& at(size_t index) {
+            return data.at(index);
         }
 
-        static void register_write(
-            RegisterFile &x,
-            Register &i,
-            Register &data)
-        {
-                register_write(x, 0, x.register_list.size(), i, data);
+        const bit& at(size_t index) const {
+            return data.at(index);
         }
 
-        struct Instruction
-        {
-                vector<bit> op;
-                vector<bit> rs1;
-                vector<bit> rs2;
-                vector<bit> rd;
-        };
-
-        static inline void full_adder(bit &s, bit &c, bit a, bit b, bit c_in)
-        {
-                s = a ^ b ^ c_in;
-                c = (a & b) | (a & c_in) | (b & c_in);
+        void push_back(const bit& b) {
+            data.push_back(b);
         }
-        struct ALU
-        {
-                static inline void full_adder(bit &s, bit &c, bit a, bit b, bit c_in)
-                {
-                        s = a ^ b ^ c_in;
-                        c = (a & b) | (a & c_in) | (b & c_in);
-                }
+    };
 
-                static inline void half_adder(bit &s, bit &c, bit a, bit b)
-                {
-                        s = a ^ b;
-                        c = a & b;
-                }
+    struct RegisterFile {
+        vector<Register> register_list;
 
-                Register alu( Instruction &current_inst, RegisterFile &reg_file)
-                {
-                        // Get values from registers using rs1 and rs2 as indices
+        RegisterFile(size_t num_registers = 32, size_t width = 32)
+            : register_list(num_registers, Register(width)) {}
 
-                        Register rs1_idx(current_inst.rs1);
-                        Register rs2_idx(current_inst.rs2);
+        // Access registers with bounds checking
+        Register& at(size_t index) {
+            return register_list.at(index);
+        }
 
-                        Register rs1_value = register_read(reg_file, 0, reg_file.register_list.size(),
-                                                           rs1_idx, rs1_idx.data.size());
-                        Register rs2_value = register_read(reg_file, 0, reg_file.register_list.size(),
-                                                           rs2_idx, rs2_idx.data.size());
+        const Register& at(size_t index) const {
+            return register_list.at(index);
+        }
 
-                        bit is_add = ~current_inst.op[1] & ~current_inst.op[0];
-                        bit is_sub = ~current_inst.op[1] & current_inst.op[0];
-                        bit is_nand = current_inst.op[1] & ~current_inst.op[0];
-                        bit is_or = current_inst.op[1] & current_inst.op[1];
+        size_t count() const {
+            return register_list.size();
+        }
+    };
 
-                        Register add_result(reg_file.register_list.at(0).data.size());
-                        bit c(0);
-                        for (bigint i = 0; i < rs2_value.data.size(); i++)
-                                full_adder(add_result.data.at(i), c, rs1_value.data.at(i), rs2_value.data.at(i), 0);
+    static Register register_read(RegisterFile &x, bigint L, bigint H, Register &i, bigint ibits) {
+        if (H <= L)
+            return Register{};
+        if (H == L + 1)
+            return x.at(L);
 
-                        Register sub_result(reg_file.register_list.at(0).data.size());
-                        for (bigint i = rs2_value.data.size(); i < rs1_value.data.size(); i++)
-                                half_adder(sub_result.data.at(i), c, rs1_value.data.at(i), c);
+        bigint splitpos = 0;
+        bigint split = 1;
+        while (L + split < H - split) {
+            splitpos += 1;
+            split *= 2;
+        }
 
-                        Register nand_result(reg_file.register_list.at(0).data.size());
-                        for (bigint i = 0; i < rs2_value.data.size(); i++)
-                                nand_result.data.at(i) = ~(rs1_value.data.at(i) & rs2_value.data.at(i));
+        if (ibits <= splitpos) {
+            return register_read(x, L, L + split, i, ibits);
+        }
 
-                        Register or_result(reg_file.register_list.at(0).data.size());
-                        for (bigint i = 0; i < rs2_value.data.size(); i++)
-                                or_result.data.at(i) = (rs1_value.data.at(i) | rs2_value.data.at(i));
+        Register result0 = register_read(x, L, L + split, i, splitpos);
+        Register result1 = register_read(x, L + split, H, i, splitpos);
 
-                        Register result(reg_file.register_list.at(0).data.size());
-                        for (bigint i = 0; i < result.data.size(); i++)
-                        {
-                                // Level 1 Muxes
-                                bit temp1 = is_add.mux(add_result.data[i], sub_result.data[i]);
-                                bit temp2 = is_nand.mux(nand_result.data[i], or_result.data[i]);
+        assert(result0.width() == result1.width());
 
-                                // Level 2 Muxes
-                                result.data[i] = (~current_inst.op[1]).mux(temp1, temp2);
-                        }
+        bit isplit = i.at(splitpos);
 
-                        return result;
-                }
-        };
+        Register result(0);
+        for (bigint r = 0; r < result0.width(); ++r) {
+            bit x0 = result0.at(r);
+            bit x1 = result1.at(r);
+            result.push_back(isplit.mux(x0, x1));
+        }
+
+        return result;
+    }
+
+    static Register register_read(RegisterFile &x, bigint L, bigint H, Register &i) {
+        return register_read(x, L, H, i, i.width());
+    }
+
+    static Register register_read(RegisterFile &x, Register &i) {
+        return register_read(x, 0, x.count(), i);
+    }
+
+    static void register_write(RegisterFile &x, bigint L, bigint H, Register &i, bigint ibits,
+                             Register &write_data, bit b = bit(1), bool top = 1) {
+        assert(x.at(0).width() == write_data.width());
+        if (H <= L)
+            return;
+        if (H == L + 1) {
+            if (top)
+                x.at(L) = write_data;
+            else
+                for (bigint r = 0; r < write_data.width(); ++r)
+                    x.at(L).at(r) = b.mux(write_data.at(r), x.at(L).at(r));
+            return;
+        }
+
+        bigint splitpos = 0;
+        bigint split = 1;
+        while (L + split < H - split) {
+            splitpos += 1;
+            split *= 2;
+        }
+
+        if (ibits <= splitpos) {
+            return register_write(x, L, L + split, i, ibits, write_data, b, top);
+        }
+
+        bit isplit = i.at(splitpos);
+        register_write(x, L, L + split, i, splitpos, write_data, top ? isplit : (b | isplit), 0);
+        register_write(x, L + split, H, i, splitpos, write_data, top ? ~isplit : b.orn(isplit), 0);
+    }
+
+    static void register_write(RegisterFile &x, bigint L, bigint H, Register &i, Register &data) {
+        register_write(x, L, H, i, i.width(), data);
+    }
+
+    static void register_write(RegisterFile &x, Register &i, Register &data) {
+        register_write(x, 0, x.count(), i, data);
+    }
+
+    struct Instruction {
+        vector<bit> op;
+        vector<bit> rs1;
+        vector<bit> rs2;
+        vector<bit> rd;
+    };
+
+    struct ALU {
+        static inline void half_adder(bit &s, bit &c, bit a, bit b) {
+            s = a ^ b;
+            c = a & b;
+        }
+
+        static inline void full_adder(bit &s, bit &c, bit a, bit b) {
+            bit t = (a ^ b);
+            s = t ^ c;
+            c = (a & b) | (c & t);
+        }
+
+        Register register_add(Register &ret, Register a, Register b, bit c_in) {
+            bit c = c_in;
+            for (bigint i = 0; i < b.width(); i++)
+                full_adder(ret.at(i), c, a.at(i), b.at(i));
+
+            for (bigint i = b.width(); i < a.width(); i++)
+                half_adder(ret.at(i), c, a.at(i), c);
+
+            if (a.width() < ret.width())
+                ret.at(a.width()) = c;
+
+            return ret;
+        }
+
+        Register register_negate(Register &ret, Register a) {
+            for (bigint i = 0; i < a.width(); i++)
+                ret.at(i) = ~a.at(i);
+            return ret;
+        }
+
+        Register alu(Instruction &current_inst, RegisterFile &reg_file) {
+            Register rs1_idx(current_inst.rs1);
+            Register rs2_idx(current_inst.rs2);
+            Register rs1_value = register_read(reg_file, 0, reg_file.count(), rs1_idx, rs1_idx.width());
+            Register rs2_value = register_read(reg_file, 0, reg_file.count(), rs2_idx, rs2_idx.width());
+
+            bit is_add = ~current_inst.op[1] & ~current_inst.op[0];
+            bit is_sub = ~current_inst.op[1] & current_inst.op[0];
+            bit is_nand = current_inst.op[1] & ~current_inst.op[0];
+            bit is_or = current_inst.op[1] & current_inst.op[0];
+
+            Register add_result(rs1_value.width());
+            register_add(add_result, rs1_value, rs2_value, 0);
+
+            Register sub_result(rs1_value.width());
+            Register negated_rs2(rs1_value.width());
+            register_negate(negated_rs2, rs2_value);
+            register_add(sub_result, rs1_value, negated_rs2, 1);
+
+            Register result(reg_file.at(0).width());
+            for (bigint i = 0; i < result.width(); i++) {
+                bit temp1 = is_add.mux(add_result.at(i), sub_result.at(i));
+                bit temp2 = is_nand.mux(add_result.at(i), sub_result.at(i));
+                result.at(i) = (~current_inst.op[1]).mux(temp1, temp2);
+            }
+
+            return result;
+        }
+    };
 };
